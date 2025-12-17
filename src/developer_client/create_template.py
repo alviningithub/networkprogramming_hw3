e7f446c7c826554bf763c806b650619791d35c3c
Alvin-nb <elliotalvin15@gmail.com>
2025-12-17 22:33:19 +0800

    add create_template

---
+++ src/developer_client/create_template.py
@@ -0,0 +1,29 @@
+import shutil
+import os
+import re
+
+str = input("please enter the desire file name :").strip()
+m = re.match(r"[A-za-z0-9\-\_]+",str).group(0)
+
+if m != str:
+    print("file name not allow")
+    os._exit(0)
+
+os.chdir("src/developer_client")
+if os.path.exists(f"games/{str}"):
+    print("path exist")
+    os._exit(0)
+
+# copy file into 
+shutil.copytree("template",f"games/{str}")
+
+# init uv
+os.chdir(f"games/{str}")
+os.system("uv init --bare")
+
+
+
+
+
+
+
